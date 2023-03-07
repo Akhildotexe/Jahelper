ceb6dd53b92932260a53229274cd7510c4cc7877
Akhil Gosala <you@example.com>
2023-03-06 20:27:53 -0500

    Added custom time between each letter typed

--- Jahelper.py
+++ Jahelper.py
@@ -10,21 +10,29 @@ root.title("Jahelper")
 label = tk.Label(root, text="Jahelper", font=("Helvetica", 30))
 label.place(relx=0.5, rely=0.2, anchor='center')
 
-note_label = tk.Label(root, text="Pls note Jahelper will start 3 SECONDS AFTER u click the Start button!")
+note_label = tk.Label(root, text="Pls note Jahelper will start 2 SECONDS AFTER u click the Start button!")
 note_label.place(relx=0.5, rely=0.3, anchor='center')
 
+delay_label = tk.Label(root, text="Above pls specify delay seconds between each letter (recommended is '0.5') ")
+delay_label.place(relx=0.5, rely=0.5, anchor='center')
+
+delay_entry = tk.Entry(root, width=5)
+delay_entry.insert(0, "0.5")
+delay_entry.place(relx=0.5, rely=0.4, anchor='center')
+
+
 def paste_clipboard():
-    time.sleep(3)
+    time.sleep(2)
     clipboard_text = pyperclip.paste()
-    keyboard.write(clipboard_text)
+    delay_time = float(delay_entry.get())
+    for letter in clipboard_text:
+        keyboard.press_and_release(letter)
+        time.sleep(delay_time)
 
 start_button = tk.Button(root, text="Start", command=paste_clipboard)
-start_button.place(relx=0.45, rely=0.5, anchor='center')
+start_button.place(relx=0.45, rely=0.6, anchor='center')
 
 quit_button = tk.Button(root, text="Quit", command=root.quit)
-quit_button.place(relx=0.59, rely=0.5, anchor='center')
+quit_button.place(relx=0.59, rely=0.6, anchor='center')
 
 root.mainloop()
-
-
-
